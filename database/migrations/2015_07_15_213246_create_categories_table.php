e2d677880e7a446bd0efcb162ac4a36aa8a6dee0
Brabant Nicolas <nbr@ppgm.fr>
2016-04-06 12:54:17 +0200

    ajout champ position à la migration

--- database/migrations/2015_07_15_213246_create_categories_table.php
+++ database/migrations/2015_07_15_213246_create_categories_table.php
@@ -15,6 +15,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function(Blueprint $table){
 			$table->increments('id');
 			$table->string('nom');
+			$table->integer('position')->default(255);
 			$table->timestamps();
 		});
     }
