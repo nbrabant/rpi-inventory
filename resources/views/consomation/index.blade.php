91474c9a994955773036d78cf177a690becf8474
Brabant Nicolas <nbr@ppgm.fr>
2016-09-24 00:14:10 +0200

    +1

--- resources/views/consomation/index.blade.php
+++ resources/views/consomation/index.blade.php
@@ -1,31 +1,30 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading clearfix">
-					@include('consomation.autocomplete')
-				</div>
 
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+<div class="col-md-12">
 
-					<div id="products-details"></div>
+	<div class="panel panel-default">
+		<div class="panel-heading clearfix">
+			@include('consomation.autocomplete')
+		</div>
+
+		<div class="panel-body">
+			@if (count($errors) > 0)
+				<div class="alert alert-danger">
+					<strong>Whoops!</strong> There were some problems with your input.<br><br>
+					<ul>
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
 				</div>
-			</div>
+			@endif
+
+			<div id="products-details"></div>
 		</div>
 	</div>
+
 </div>
 
 @endsection
