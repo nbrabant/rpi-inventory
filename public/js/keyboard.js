df02a5ed8fb49112a4a0cec1e765fd0c9aef3289
Brabant Nicolas <nbr@ppgm.fr>
2016-10-18 02:19:16 +0200

    patch

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+	var availableTags = ["ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++", "Clojure",
+		"COBOL", "ColdFusion", "Erlang", "Fortran", "Groovy", "Haskell", "Java", "JavaScript",
+		"Lisp", "Perl", "PHP", "Python", "Ruby", "Scala", "Scheme" ];
+
 	$('.text-keyboard')
 		.keyboard({ layout: 'qwerty' })
 		.autocomplete({
