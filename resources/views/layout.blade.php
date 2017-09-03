22e4a6af60a5aa7815a23d49f7996e64d3fc6b06
nbrabant <brabantnicolas59@gmail.com>
2017-09-03 17:01:09 +0200

    update to laravel 5.5

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,28 +1,31 @@
 <!doctype html>
 <html lang="en">
 
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <head>
+        <title>RPI Inventory</title>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="google" value="notranslate">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>RPI Inventory</title>
+        <link rel="icon" type="image/png" href="assets/img/favicon.ico" />
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
+    </head>
 
-    <style src="{{ elixir('css/app.css', '') }}"></style>
-  </head>
+    <body>
 
-  <body>
-    <app>
-  		<div class="loading-placeholder-wrapper">
-  		    <div class="loading-placeholder-inner-wrapper">
-  		        <span class="loading-placeholder">Loading...</span>
-  		    </div>
-  		</div>
-  	</app>
-  </body>
+        @yield('content')
 
-  <!--   Core JS Files   -->
-  <script src="{{ elixir('js/app.js', '') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
+
+        <app>
+            <div class="loading-placeholder-wrapper">
+                <div class="loading-placeholder-inner-wrapper">
+                    <span class="loading-placeholder">Loading...</span>
+                </div>
+            </div>
+        </app>
+    </body>
+
 </html>
