bd415a910a3e1bce7507250a31f5f4b039b1fc4c
nbrabant <brabantnicolas59@gmail.com>
2018-09-09 23:26:41 +0200

    Add Pdf package dependencies

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\DomPDF\ServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
