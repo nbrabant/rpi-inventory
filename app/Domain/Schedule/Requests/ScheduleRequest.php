2a271257da378b7b8800a1dd4ca75ea088b31625
nbrabant <brabantnicolas59@gmail.com>
2021-11-15 23:28:45 +0100

    feat(Request) : Add schedule request
    - see above

---
+++ app/Domain/Schedule/Requests/ScheduleRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Domain\Schedule\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ScheduleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'recipe_id'     => 'required_if:type_schedule,recette,numeric',
+            'user_id'       => 'numeric',
+            'type_schedule' => 'required|in:recette,rendezvous,planning',
+            'title'         => 'required|string',
+            'details'       => 'string',
+            'start_at'      => 'required|date_format:Y-m-d H:i:s',
+            'end_at'        => 'required|date_format:Y-m-d H:i:s',
+            'all_day'       => 'required|boolean'
+        ];
+    }
+}
