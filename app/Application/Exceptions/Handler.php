865e515d75f97572e4e78340591fb699d8ba8c98
nbrabant <brabantnicolas59@gmail.com>
2021-11-23 22:58:40 +0100

    chore(upgrade) : bump laravel version to 7.x
    - see above

--- app/Application/Exceptions/Handler.php
+++ app/Application/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Application\Exceptions;
 
-use Exception;
+Use Throwable;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
@@ -27,10 +27,11 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param Throwable $exception
      * @return void
+     * @throws Throwable
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -38,11 +39,12 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param Throwable $exception
      * @return \Illuminate\Http\Response
+     * @throws Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
