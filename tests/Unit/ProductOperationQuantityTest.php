607ac3431b547e36f269153a40fddeccbb777547
nbrabant <brabantnicolas59@gmail.com>
2021-11-26 23:38:19 +0100

    chore(Tests) : remove old non passing test
    - see above

--- tests/Unit/ProductOperationQuantityTest.php
+++ tests/Unit/ProductOperationQuantityTest.php
@@ -10,11 +10,5 @@ use App\Domain\Stock\Repositories\OperationRepository as Operation;
 
 class ProductOperationQuantityTest extends TestCase
 {
-
-    public function testQuantityCounter()
-    {
-        $this->assertTrue(true);
-        $operation = new Operation(app());
-        $this->assertInternalType( 'int', $operation->countQuantityByProduct(2) );
-    }
+    
 }
