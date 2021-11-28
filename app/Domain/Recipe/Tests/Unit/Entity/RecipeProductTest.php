6f2389f9908eaa499f3412702882a169e7317878
nbrabant <brabantnicolas59@gmail.com>
2021-11-28 23:48:46 +0100

    chore(Unit test) : Testing recipe product quantity ratio calculation
    - see above

--- app/Domain/Recipe/Tests/Unit/Entity/RecipeProductTest.php
+++ app/Domain/Recipe/Tests/Unit/Entity/RecipeProductTest.php
@@ -2,7 +2,48 @@
 
 namespace App\Domain\Recipe\Tests\Unit\Entity;
 
-class RecipeProductTest
-{
+use App\Domain\Recipe\Entities\RecipeProduct;
+use PHPUnit\Framework\TestCase;
 
+class RecipeProductTest extends TestCase
+{
+    /**
+     * Test correct quantity ratio conversion
+     *
+     * @dataProvider ratioProvider
+     *
+     * @param string $unite
+     * @param int $quantity
+     * @param float $expected
+     */
+    public function testGetQuantityShouldReturnQuantityRatio(string $unite, int $quantity, float $expected)
+    {
+        $recipeProduct = new RecipeProduct([
+            'unit' => $unite,
+            'quantity' => $quantity
+        ]);
+
+        self::assertEquals($expected, $recipeProduct->getQuantity());
+    }
+
+    /**
+     * Provide recipe product informations for test
+     *
+     * @return mixed[]
+     */
+    public static function ratioProvider(): array
+    {
+        return [
+            ['centilitre', 2, 0.2],
+            ['cuilliere_cafe', 2, 8.0],
+            ['cuilliere_dessert', 2, 16.0],
+            ['cuilliere_soupe', 2, 24.0],
+            ['verre_liqueur', 2, 0.06],
+            ['tasse_cafe', 2, 0.2],
+            ['verre_moutarde', 2, 0.3],
+            ['grand_verre', 2, 0.5],
+            ['bol', 2, 0.7],
+            ['louche', 2, 2],
+        ];
+    }
 }
