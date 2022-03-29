ad4c4d0504de9380ef42fc02ce2253ffc10ffa3e
nbrabant <brabantnicolas59@gmail.com>
2022-03-29 22:53:11 +0200

    chore(Search criteria) : Extract dateFilter criteria
    - see above

--- app/Infrastructure/Entities/ValueObject/StartDate.php
+++ app/Infrastructure/Entities/ValueObject/StartDate.php
@@ -2,7 +2,34 @@
 
 namespace App\Infrastructure\Entities\ValueObject;
 
+use Carbon\Carbon;
+
 class StartDate
 {
+    /**
+     * @var Carbon $date
+     */
+    public Carbon $date;
 
+    public function __construct(Carbon $date)
+    {
+        $this->date = $date->startOfDay();
+    }
+
+    /**
+     * @param string $date
+     * @return static
+     */
+    public static function fromString(string $date): self
+    {
+        return new static(Carbon::parse($date));
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->date->format('Y-m-d H:i:s');
+    }
 }
