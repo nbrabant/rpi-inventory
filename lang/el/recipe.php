dda45d97073c78d222a6b1fcd69fc62623532b1e
Konstantinos Megas <nextdoorpanda@gmail.com>
2022-10-12 12:59:33 +0300

    adds greek translation

---
+++ lang/el/recipe.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'centilitre' => '{0,1} :quantity εκατοστόλιτρο|{2,*} :quantity εκατοστόλιτρα',
+    'cuilliere_cafe' => '{0,1} :quantity κουταλάκι του καφέ|{2,*} :quantity κουταλάκια του καφέ',
+    'cuilliere_dessert' => '{0,1} :quantity κουταλάκι του γλυκού|{2,*} :quantity κουταλάκια του γλυκού',
+    'cuilliere_soupe' => '{0,1} :quantity κουτάλι της σούπας|{2,*} :quantity κουτάλια της σούπας',
+    'verre_liqueur' => '{0,1} :quantity σφηνοπότηρο|{2,*} :quantity σφηνοπότηρα',
+    'tasse_cafe' => '{0,1} :quantity φλυντζάνι του καφέ|{2,*} :quantity φλυντζάνια του καφέ',
+    'verre_moutarde' => '{0,1} :quantity ποτήρι νερού|{2,*} :quantity ποτήρια νερού',
+    'grand_verre' => '{0,1} :quantity μεγάλο ποτήρι|{2,*} :quantity μεγάλα ποτήρια',
+    'bol' => '{0,1} :quantity μπολ|{2,*} :quantity μπολ',
+    'louche' => '{0,1} :quantity κουτάλα|{2,*} :quantity κουτάλες',
+];
