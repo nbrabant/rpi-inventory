e1ef33e54206a08bf977e15106eee3c823f587bb
David Angenendt <david.angenendt@surplex.com>
2022-10-15 23:11:35 +0200

    #25 Tests Added Cart Test fixed delete request from frontend

---
+++ app/Domain/Cart/Tests/Feature/CartTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Domain\Cart\Tests\Feature;
+
+use App\Domain\Cart\Entities\Cart;
+use App\Domain\Cart\Entities\ProductLine;
+use App\Domain\Stock\Entities\Product;
+use Database\Seeders\ProductTableSeeder;
+use Tests\TestCase;
+
+class CartTest extends TestCase
+{
+    private int $productId = 1;
+
+    protected function setUp(): void
+    {
+        (new ProductTableSeeder())->run();
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+
+        Product::truncate();
+        Cart::truncate();
+        ProductLine::truncate();
+    }
+
+    public function test_add_product_to_cart()
+    {
+        $postData = ['product_id' => $this->productId, 'quantity' => 0];
+        $resp = $this->post('/api/cartproducts', $postData);
+        $decoded = $resp->decodeResponseJson();
+        $this->assertArrayHasKey('product_lines', $decoded, 'Missing product lines key');
+        $this->assertCount(1, $decoded['product_lines'], 'Seems like a product_lines count');
+        $this->assertArrayHasKey('id', $decoded['product_lines'][0], 'Missing id');
+        // clean up cart
+        $resp = $this->delete(sprintf('/api/cartproducts/%s', $this->productId), ['product_id' => $this->productId]);
+        $this->assertEquals(200, $resp->status(), 'Wrong status code returned for deletion');
+        $decoded = $resp->decodeResponseJson();
+        $this->assertArrayHasKey('id', $decoded, 'No id returned after deletion');
+    }
+
+    public function test_add_and_update_product_in_cart()
+    {
+        $newQuantity = 666;
+        $postData = ['product_id' => $this->productId, 'quantity' => 0];
+        $this->post('/api/cartproducts', $postData);
+        $resp = $this->patch('/api/cartproducts/1', [
+            'cart_id' => 1,
+            'id' => 1,
+            'product_id' => $this->productId,
+            'quantity' => $newQuantity
+        ]);
+        $decoded = $resp->decodeResponseJson();
+        $this->assertArrayHasKey('product_lines', $decoded, 'missing index product_lines');
+        $this->assertCount(1, $decoded['product_lines'], 'Wrong count');
+        $this->assertEquals($this->productId, $decoded['product_lines'][0]['product_id']);
+        $this->assertEquals($newQuantity, $decoded['product_lines'][0]['quantity']);
+
+        // clean up cart
+        $resp = $this->delete(sprintf('/api/cartproducts/%s', $this->productId), ['product_id' => $this->productId]);
+        $this->assertEquals(200, $resp->status(), 'Wrong status code returned for deletion');
+        $decoded = $resp->decodeResponseJson();
+        $this->assertArrayHasKey('id', $decoded, 'No id returned after deletion');
+    }
+
+    public function test_add_and_finish_cart()
+    {
+        $postData = ['product_id' => $this->productId, 'quantity' => 0];
+        $resp = $this->post('/api/cartproducts', $postData);
+        $decoded = $resp->decodeResponseJson();
+        $cartId = $decoded['product_lines'][0]['cart_id'];
+        $resp = $this->patch("/api/carts/$cartId", ['finished' => true]);
+        $decoded = $resp->decodeResponseJson();
+        $this->assertCount(0, $decoded['product_lines']);
+    }
+}
